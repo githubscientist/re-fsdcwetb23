4b317678b805bfa3207f4df36741c5fd0fbc4b03
githubscientist <sathish.datascientist@gmail.com>
2025-06-28 12:07:00 +0530

    creating multiple componennts

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
-function App() {
+function Greeting() {
   return (
     <>
       <h1>Hello, World!</h1>
       <p>This is my First Component</p>
+    </>
+  )
+}
+
+function App() {
+  return (
+    <>
+      <Greeting />
+      <Greeting />
+      <Greeting />
     </>
   )
 }
