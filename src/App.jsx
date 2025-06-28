5479f445557a193b31b5574855ace5141c03a53a
githubscientist <sathish.datascientist@gmail.com>
2025-06-28 12:11:16 +0530

    moving Greeting component to its own file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,4 @@
-function Greeting() {
-  return (
-    <>
-      <h1>Hello, World!</h1>
-      <p>This is my First Component</p>
-    </>
-  )
-}
+import Greeting from "./components/Greeting";
 
 function App() {
   return (
---
+++ src/components/Greeting.jsx
@@ -0,0 +1,10 @@
+function Greeting() {
+    return (
+        <>
+            <h1>Hello, World!</h1>
+            <p>This is my First Component</p>
+        </>
+    )
+}
+
+export default Greeting;
