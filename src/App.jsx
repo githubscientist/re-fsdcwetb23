d6c8f3b8ece1d03723f32042b69236ccd8a61a6d
githubscientist <sathish.datascientist@gmail.com>
2025-07-05 13:14:46 +0530

    rendering api data to jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,27 +9,33 @@ import { useEffect, useState } from "react";
 
 const App = () => {
 
-  const [state, setState] = useState(0);
-
-  // this runs after the component mounts and after every re-render
-  // useEffect(() => {
-  //   fetch(`https://685ac3af9f6ef9611157b188.mockapi.io/todos`)
-  //     .then(response => response.json())
-  //     .then(data => console.log(data))
-  // });
+  const [todos, setTodos] = useState([]);
 
   useEffect(() => {
     fetch(`https://685ac3af9f6ef9611157b188.mockapi.io/todos`)
       .then(response => response.json())
-      .then(data => console.log(data))
-  }, [state]);
+      .then(data => setTodos(data))
+  }, []);
 
   return (
     <div>
-      <h1>App</h1>
-      <button onClick={() => setState(state + 1)}>Re-Render Component</button>
+      <h1>Todos</h1>
+      <ul>
+        {
+          todos.map(todo => {
+            return <li key={todo.id}>{todo.content}</li>;
+          })
+        }
+      </ul>
     </div>
   )
 }
 
 export default App;
+
+/*
+  Component -> UI/HTML Elements + JS Logic
+
+               |
+              Products Data -> via data fetching (API) -> set the data to the state -> component re-renders -> latest data is displayed
+*/
