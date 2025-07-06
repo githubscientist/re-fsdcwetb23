d2ae3b7d63d342a66dd1314732ee2d6d305a0dec
githubscientist <sathish.datascientist@gmail.com>
2025-07-06 11:20:26 +0530

    conditional rendering --using ternary operator

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,9 +1,6 @@
 const TodoItem = ({ todo }) => {
-    if (todo.isCompleted) {
-        return <li>{todo.content} {'✅'}</li>
-    } else {
-        return <li>{todo.content}</li>
-    }
+
+    return todo.isCompleted ? <li>{todo.content} {'✅'}</li> : <li>{todo.content}</li>;
 }
 
 export default TodoItem;
