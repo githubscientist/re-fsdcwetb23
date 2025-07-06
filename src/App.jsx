76939e1b9e278609d21e9a2ea8d07b66e940d278
githubscientist <sathish.datascientist@gmail.com>
2025-07-06 10:43:30 +0530

    rendering data into jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,6 @@
-/*
-    useEffect Hook: 
-
-    - Used to perform side effects in function components.
-    - side effects can include data fetching, subscriptions, or manually changing the DOM.
-*/
-
 import { useEffect, useState } from "react";
 
+// API URL: https://685ac3af9f6ef9611157b188.mockapi.io/todos
 const App = () => {
 
   const [todos, setTodos] = useState([]);
@@ -14,28 +8,25 @@ const App = () => {
   useEffect(() => {
     fetch(`https://685ac3af9f6ef9611157b188.mockapi.io/todos`)
       .then(response => response.json())
-      .then(data => setTodos(data))
+      .then(data => setTodos(data));
   }, []);
 
+  useEffect(() => {
+    console.log(todos);
+  }, [todos]);
+
   return (
-    <div>
+    <>
       <h1>Todos</h1>
       <ul>
         {
-          todos.map(todo => {
-            return <li key={todo.id}>{todo.content}</li>;
-          })
+          todos.map((todo, index) => (
+            <li key={index}>{todo.content}</li>
+          ))
         }
       </ul>
-    </div>
+    </>
   )
 }
 
 export default App;
-
-/*
-  Component -> UI/HTML Elements + JS Logic
-
-               |
-              Products Data -> via data fetching (API) -> set the data to the state -> component re-renders -> latest data is displayed
-*/
