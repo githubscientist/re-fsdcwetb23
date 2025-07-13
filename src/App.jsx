d7ba8225ab2b774704474308dbe3033ca9ff8535
githubscientist <sathish.datascientist@gmail.com>
2025-07-13 11:15:50 +0530

    upgraded from v6 to v7 react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,34 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router";
 
-// 1. Create a router object.
-const router = createBrowserRouter([
+const routes = [
   {
     path: "/",
     element: <h1>Hello World!</h1>
   },
-]);
+  {
+    path: "/about",
+    element: <h1>About Us</h1>
+  }
+];
+
+// 1. Create a router object.
+const router = createBrowserRouter(routes, {
+  future: {
+    v7_relativeSplatPath: true,
+    v7_fetcherPersist: true,
+    v7_normalizeFormMethod: true,
+    v7_partialHydration: true,
+    v7_skipActionErrorRevalidation: true,
+  },
+});
 
 const App = () => {
   return (
     <RouterProvider
       router={router}
+      future={{
+        v7_startTransition: true,
+      }}
     />
   )
 }
