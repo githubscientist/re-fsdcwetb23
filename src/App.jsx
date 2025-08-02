168073fb37b07620c616a76dae2f4bcb7d1d18a0
githubscientist <sathish.datascientist@gmail.com>
2025-08-02 12:47:57 +0530

    dislike feature added

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
 import { useDispatch, useSelector } from "react-redux";
-import { like, selectLikes } from "./redux/features/reactionSlice";
+import { dislike, like, selectDislikes, selectLikes } from "./redux/features/reactionSlice";
 
 const App = () => {
 
   const likes = useSelector(selectLikes);
+  const dislikes = useSelector(selectDislikes);
   const dispatch = useDispatch();
 
   const handleLike = () => {
     dispatch(like());
   }
 
+  const handleDislike = () => {
+    dispatch(dislike());
+  }
+
   return (
     <div>
       <h1>Likes: {likes}</h1>
       <button onClick={handleLike}>Like</button>
+
+      <h1>Dislikes: {dislikes}</h1>
+      <button onClick={handleDislike}>Dislike</button>
     </div>
   )
 }
--- src/redux/features/reactionSlice.js
+++ src/redux/features/reactionSlice.js
@@ -4,16 +4,21 @@ export const reactionSlice = createSlice({
     name: "reaction",
     initialState: {
         likes: 0,
+        dislikes: 0
     },
     reducers: {
         like: (state) => {
             state.likes += 1;
+        },
+        dislike: (state) => {
+            state.dislikes += 1;
         }
     }
 });
 
-export const { like } = reactionSlice.actions;
+export const { like, dislike } = reactionSlice.actions;
 
 export const selectLikes = (state) => state.reaction.likes;
+export const selectDislikes = (state) => state.reaction.dislikes;
 
 export default reactionSlice.reducer;
