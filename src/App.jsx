d07ceaf2c76a6831f4e3b22a142dfc423b3436ab
githubscientist <sathish.datascientist@gmail.com>
2025-08-02 10:51:45 +0530

    state management --useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
+import { useState } from "react";
+
 const App = () => {
+
+  const [likes, setLikes] = useState(0);
+
+  const handleLike = () => {
+    setLikes(likes + 1);
+  }
+
   return (
-    <div>Redux Demo!</div>
+    <div>
+      <h1>Likes: {likes}</h1>
+      <button onClick={handleLike}>Like</button>
+    </div>
   )
 }
 
